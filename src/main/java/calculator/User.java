02d5789a45aa1d17edfa6c57aae0d05ec7041180
Enoch <walkerhaozheng@gmail.com>
2019-10-28 23:17:00 -0700

    Updated User class

--- src/main/java/calculator/User.java
+++ src/main/java/calculator/User.java
@@ -3,10 +3,10 @@ package calculator;
 public class User {
 
     public static void main(String[] args) {
-        // User user = new User();
-        // Calculator calculator = new Calculator();
-        // user.start(calculator);
-        System.out.println("Lab3 is awesome!");
+        User user = new User();
+        Calculator calculator = new Calculator();
+        user.start(calculator);
+        // System.out.println("Lab3 is awesome!");
     }
 
     public void start(Calculator calculator) {
